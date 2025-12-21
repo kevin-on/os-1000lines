2154bc9f0901aab557cbcdc414180cb89e62c1f0
Kevin On <40454531+kevin-on@users.noreply.github.com>
2025-12-21 12:25:19 +0900

    06. C Standard Library

--- common.c
+++ common.c
@@ -2,7 +2,42 @@
 
 void putchar(char ch);
 
-// printf that supports %s, %d, %x
+void *memset(void *buf, char c, size_t n) {
+  uint8_t *p = (uint8_t *) buf;
+  while (n--)
+      *p++ = c;
+  return buf;
+}
+
+void *memcpy(void *dst, const void *src, size_t n) {
+  uint8_t *d = (uint8_t *) dst;
+  const uint8_t *s = (const uint8_t *) src;
+  while (n--)
+      *d++ = *s++;
+  return dst;
+}
+
+char *strcpy(char *dst, const char *src) {
+  char *d = dst;
+  while (*src)
+    *d++ = *src++;
+  *d = '\0';
+  return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && *s2) {
+    if (*s1 != *s2)
+      break;
+    s1++;
+    s2++;
+  }
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
+
+/**
+ * printf that supports %s, %d, %x
+ */
 void printf(const char *fmt, ...) {
     va_list vargs;
     va_start(vargs, fmt);
