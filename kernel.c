2b6e344a11ebe8c1b8a7ebc8022d0d8525b12b20
Kevin On <40454531+kevin-on@users.noreply.github.com>
2025-12-23 13:46:02 +0900

    10. Process

--- kernel.c
+++ kernel.c
@@ -34,77 +34,82 @@ __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void) {
     __asm__ __volatile__(
-        "csrw sscratch, sp\n"
-        "addi sp, sp, -4 * 31\n"
-        "sw ra,  4 * 0(sp)\n"
-        "sw gp,  4 * 1(sp)\n"
-        "sw tp,  4 * 2(sp)\n"
-        "sw t0,  4 * 3(sp)\n"
-        "sw t1,  4 * 4(sp)\n"
-        "sw t2,  4 * 5(sp)\n"
-        "sw t3,  4 * 6(sp)\n"
-        "sw t4,  4 * 7(sp)\n"
-        "sw t5,  4 * 8(sp)\n"
-        "sw t6,  4 * 9(sp)\n"
-        "sw a0,  4 * 10(sp)\n"
-        "sw a1,  4 * 11(sp)\n"
-        "sw a2,  4 * 12(sp)\n"
-        "sw a3,  4 * 13(sp)\n"
-        "sw a4,  4 * 14(sp)\n"
-        "sw a5,  4 * 15(sp)\n"
-        "sw a6,  4 * 16(sp)\n"
-        "sw a7,  4 * 17(sp)\n"
-        "sw s0,  4 * 18(sp)\n"
-        "sw s1,  4 * 19(sp)\n"
-        "sw s2,  4 * 20(sp)\n"
-        "sw s3,  4 * 21(sp)\n"
-        "sw s4,  4 * 22(sp)\n"
-        "sw s5,  4 * 23(sp)\n"
-        "sw s6,  4 * 24(sp)\n"
-        "sw s7,  4 * 25(sp)\n"
-        "sw s8,  4 * 26(sp)\n"
-        "sw s9,  4 * 27(sp)\n"
-        "sw s10, 4 * 28(sp)\n"
-        "sw s11, 4 * 29(sp)\n"
+      "csrrw sp, sscratch, sp\n"
 
-        "csrr a0, sscratch\n"
-        "sw a0, 4 * 30(sp)\n"
+      "addi sp, sp, -4 * 31\n"
+      "sw ra,  4 * 0(sp)\n"
+      "sw gp,  4 * 1(sp)\n"
+      "sw tp,  4 * 2(sp)\n"
+      "sw t0,  4 * 3(sp)\n"
+      "sw t1,  4 * 4(sp)\n"
+      "sw t2,  4 * 5(sp)\n"
+      "sw t3,  4 * 6(sp)\n"
+      "sw t4,  4 * 7(sp)\n"
+      "sw t5,  4 * 8(sp)\n"
+      "sw t6,  4 * 9(sp)\n"
+      "sw a0,  4 * 10(sp)\n"
+      "sw a1,  4 * 11(sp)\n"
+      "sw a2,  4 * 12(sp)\n"
+      "sw a3,  4 * 13(sp)\n"
+      "sw a4,  4 * 14(sp)\n"
+      "sw a5,  4 * 15(sp)\n"
+      "sw a6,  4 * 16(sp)\n"
+      "sw a7,  4 * 17(sp)\n"
+      "sw s0,  4 * 18(sp)\n"
+      "sw s1,  4 * 19(sp)\n"
+      "sw s2,  4 * 20(sp)\n"
+      "sw s3,  4 * 21(sp)\n"
+      "sw s4,  4 * 22(sp)\n"
+      "sw s5,  4 * 23(sp)\n"
+      "sw s6,  4 * 24(sp)\n"
+      "sw s7,  4 * 25(sp)\n"
+      "sw s8,  4 * 26(sp)\n"
+      "sw s9,  4 * 27(sp)\n"
+      "sw s10, 4 * 28(sp)\n"
+      "sw s11, 4 * 29(sp)\n"
 
-        "mv a0, sp\n"
-        "call handle_trap\n"
+      "csrr a0, sscratch\n"
+      "sw a0, 4 * 30(sp)\n"
 
-        "lw ra,  4 * 0(sp)\n"
-        "lw gp,  4 * 1(sp)\n"
-        "lw tp,  4 * 2(sp)\n"
-        "lw t0,  4 * 3(sp)\n"
-        "lw t1,  4 * 4(sp)\n"
-        "lw t2,  4 * 5(sp)\n"
-        "lw t3,  4 * 6(sp)\n"
-        "lw t4,  4 * 7(sp)\n"
-        "lw t5,  4 * 8(sp)\n"
-        "lw t6,  4 * 9(sp)\n"
-        "lw a0,  4 * 10(sp)\n"
-        "lw a1,  4 * 11(sp)\n"
-        "lw a2,  4 * 12(sp)\n"
-        "lw a3,  4 * 13(sp)\n"
-        "lw a4,  4 * 14(sp)\n"
-        "lw a5,  4 * 15(sp)\n"
-        "lw a6,  4 * 16(sp)\n"
-        "lw a7,  4 * 17(sp)\n"
-        "lw s0,  4 * 18(sp)\n"
-        "lw s1,  4 * 19(sp)\n"
-        "lw s2,  4 * 20(sp)\n"
-        "lw s3,  4 * 21(sp)\n"
-        "lw s4,  4 * 22(sp)\n"
-        "lw s5,  4 * 23(sp)\n"
-        "lw s6,  4 * 24(sp)\n"
-        "lw s7,  4 * 25(sp)\n"
-        "lw s8,  4 * 26(sp)\n"
-        "lw s9,  4 * 27(sp)\n"
-        "lw s10, 4 * 28(sp)\n"
-        "lw s11, 4 * 29(sp)\n"
-        "lw sp,  4 * 30(sp)\n"
-        "sret\n"
+      "addi a0, sp, 4 * 31\n"
+      "csrw sscratch, a0\n"
+
+      "mv a0, sp\n"
+      "call handle_trap\n"
+
+      "lw ra,  4 * 0(sp)\n"
+      "lw gp,  4 * 1(sp)\n"
+      "lw tp,  4 * 2(sp)\n"
+      "lw t0,  4 * 3(sp)\n"
+      "lw t1,  4 * 4(sp)\n"
+      "lw t2,  4 * 5(sp)\n"
+      "lw t3,  4 * 6(sp)\n"
+      "lw t4,  4 * 7(sp)\n"
+      "lw t5,  4 * 8(sp)\n"
+      "lw t6,  4 * 9(sp)\n"
+      "lw a0,  4 * 10(sp)\n"
+      "lw a1,  4 * 11(sp)\n"
+      "lw a2,  4 * 12(sp)\n"
+      "lw a3,  4 * 13(sp)\n"
+      "lw a4,  4 * 14(sp)\n"
+      "lw a5,  4 * 15(sp)\n"
+      "lw a6,  4 * 16(sp)\n"
+      "lw a7,  4 * 17(sp)\n"
+      "lw s0,  4 * 18(sp)\n"
+      "lw s1,  4 * 19(sp)\n"
+      "lw s2,  4 * 20(sp)\n"
+      "lw s3,  4 * 21(sp)\n"
+      "lw s4,  4 * 22(sp)\n"
+      "lw s5,  4 * 23(sp)\n"
+      "lw s6,  4 * 24(sp)\n"
+      "lw s7,  4 * 25(sp)\n"
+      "lw s8,  4 * 26(sp)\n"
+      "lw s9,  4 * 27(sp)\n"
+      "lw s10, 4 * 28(sp)\n"
+      "lw s11, 4 * 29(sp)\n"
+      "lw sp,  4 * 30(sp)\n"
+
+      "sret\n"
     );
 }
 
@@ -130,6 +135,122 @@ paddr_t alloc_pages(uint32_t n) {
 
   memset((void *) paddr, 0, n * PAGE_SIZE);
   return paddr;
+}
+
+__attribute__((naked))
+void switch_context(uint32_t *prev_sp,
+                    uint32_t *next_sp) {
+  __asm__ __volatile__(
+  // Save callee-saved registers onto the current process's stack.
+  "addi sp, sp, -13 * 4\n" // Allocate stack space for 13 4-byte registers
+  "sw ra,  0  * 4(sp)\n"   // Save callee-saved registers only
+  "sw s0,  1  * 4(sp)\n"
+  "sw s1,  2  * 4(sp)\n"
+  "sw s2,  3  * 4(sp)\n"
+  "sw s3,  4  * 4(sp)\n"
+  "sw s4,  5  * 4(sp)\n"
+  "sw s5,  6  * 4(sp)\n"
+  "sw s6,  7  * 4(sp)\n"
+  "sw s7,  8  * 4(sp)\n"
+  "sw s8,  9  * 4(sp)\n"
+  "sw s9,  10 * 4(sp)\n"
+  "sw s10, 11 * 4(sp)\n"
+  "sw s11, 12 * 4(sp)\n"
+
+  // Switch the stack pointer.
+  "sw sp, (a0)\n"         // *prev_sp = sp;
+  "lw sp, (a1)\n"         // Switch stack pointer (sp) here
+
+  // Restore callee-saved registers from the next process's stack.
+  "lw ra,  0  * 4(sp)\n"  // Restore callee-saved registers only
+  "lw s0,  1  * 4(sp)\n"
+  "lw s1,  2  * 4(sp)\n"
+  "lw s2,  3  * 4(sp)\n"
+  "lw s3,  4  * 4(sp)\n"
+  "lw s4,  5  * 4(sp)\n"
+  "lw s5,  6  * 4(sp)\n"
+  "lw s6,  7  * 4(sp)\n"
+  "lw s7,  8  * 4(sp)\n"
+  "lw s8,  9  * 4(sp)\n"
+  "lw s9,  10 * 4(sp)\n"
+  "lw s10, 11 * 4(sp)\n"
+  "lw s11, 12 * 4(sp)\n"
+  "addi sp, sp, 13 * 4\n"  // We've popped 13 4-byte registers from the stack
+  "ret\n"
+  );
+}
+
+struct process procs[PROCS_MAX]; // All process control structures.
+
+struct process *create_process(uint32_t pc) {
+    // Find an unused process control structure.
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    // Stack callee-saved registers. These register values will be restored in
+    // the first context switch in switch_context.
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    // Initialize fields.
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
+struct process *current_proc;
+struct process *idle_proc;
+
+void yield(void) {
+  // Search for a runnable process
+  struct process *next = idle_proc;
+  for (int i = 0; i < PROCS_MAX; i++) {
+    struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+    if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+      next = proc;
+      break;
+    }
+  }
+
+  printf("yield: current process is %d\n", current_proc->pid);
+  printf("yield: next process is %d\n", next->pid);
+
+  // If current process is the only runnable process, return and continue processing
+  if (next == current_proc)
+    return;
+
+  __asm__ __volatile__(
+    "csrw sscratch, %[sscratch]\n"
+    :
+    : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+  );
+
+  // Switch context and update current process
+  struct process *prev = current_proc;
+  current_proc = next;
+  switch_context(&prev->sp, &next->sp);
 }
 
 
@@ -176,14 +297,78 @@ void test_alloc_pages(void) {
   printf("alloc_pages test: paddr1=%x\n", paddr1);
 }
 
+/* Test context switching */
+void delay(void) {
+  for (int i = 0; i < 30000000; i++)
+      __asm__ __volatile__("nop"); // do nothing
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) {
+  printf("starting process A\n");
+  while (1) {
+      uint32_t current_sp;
+      __asm__ __volatile__("mv %0, sp" : "=r"(current_sp));
+      if (current_sp < (uint32_t) proc_a->stack || current_sp > (uint32_t) proc_a->stack + sizeof(proc_a->stack)) {
+        // Temporarily disable panic
+        // PANIC("proc_a: current sp is not inside the process's stack");
+      }
+
+      putchar('A');
+      yield();
+  }
+}
+
+void proc_b_entry(void) {
+  printf("starting process B\n");
+  while (1) {
+      uint32_t current_sp;
+      __asm__ __volatile__("mv %0, sp" : "=r"(current_sp));
+      if (current_sp < (uint32_t) proc_b->stack || current_sp > (uint32_t) proc_b->stack + sizeof(proc_b->stack)) {
+        PANIC("proc_b: current sp is not inside the process's stack");
+      }
+
+      putchar('B');
+      yield();
+  }
+}
+
+void test_context_switching(void) {
+  proc_a = create_process((uint32_t) proc_a_entry);
+  proc_b = create_process((uint32_t) proc_b_entry);
+  // proc_a_entry();
+  yield();
+  PANIC("switched to idle process");
+}
+
+
+void idle_entry(void) {
+  for (;;) {
+    yield();
+    __asm__ __volatile__("wfi");
+  }
+}
+
+/**
+ * Kernel main function.
+ */
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+
   WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+  // Initialize idle process
+  idle_proc = create_process((uint32_t) idle_entry);
+  idle_proc->pid = 0;
+  current_proc = idle_proc;
 
   // test_printf();
   // test_common_functions();
   // test_exceptions();
-  test_alloc_pages();
+  // test_alloc_pages();
+  test_context_switching();
 
   for (;;) {
     __asm__ __volatile__("wfi");
--- kernel.h
+++ kernel.h
@@ -7,6 +7,25 @@ struct sbiret {
     long value;
 };
 
+#define PANIC(fmt, ...)                                                        \
+    do {                                                                       \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {}                                                           \
+    } while (0)
+
+#define READ_CSR(reg)                                                          \
+    ({                                                                         \
+        unsigned long __tmp;                                                   \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
+        __tmp;                                                                 \
+    })
+
+#define WRITE_CSR(reg, value)                                                  \
+    do {                                                                       \
+        uint32_t __tmp = (value);                                              \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
+    } while (0)
+
 struct trap_frame {
     uint32_t ra;
     uint32_t gp;
@@ -41,21 +60,16 @@ struct trap_frame {
     uint32_t sp;
 } __attribute__((packed));
 
-#define PANIC(fmt, ...)                                                        \
-    do {                                                                       \
-        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
-        while (1) {}                                                           \
-    } while (0)
 
-#define READ_CSR(reg)                                                          \
-    ({                                                                         \
-        unsigned long __tmp;                                                   \
-        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
-        __tmp;                                                                 \
-    })
+/* Process management */
+#define PROCS_MAX 8       // Maximum number of processes
 
-#define WRITE_CSR(reg, value)                                                  \
-    do {                                                                       \
-        uint32_t __tmp = (value);                                              \
-        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
-    } while (0)
+#define PROC_UNUSED   0   // Unused process control structure
+#define PROC_RUNNABLE 1   // Runnable process
+
+struct process {
+    int pid;             // Process ID
+    int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+    vaddr_t sp;          // Stack pointer
+    uint8_t stack[8192]; // Kernel stack
+};
